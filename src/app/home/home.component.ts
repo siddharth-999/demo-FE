ac0e43b14f88b9987e5ffe695aa849e779ff03aa
Siddharth Rakholiya <53362121+siddharth-999@users.noreply.github.com>
2021-04-15 10:29:36 +0530

    added relationship update changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,15 +9,18 @@ import { AuthService } from '../services/auth.service';
 export class HomeComponent implements OnInit {
   data: [];
   relative_data = [];
+  public show: boolean = false;
+  public Edit: any = 'Show';
+
   constructor(
     private authenticationService: AuthService,
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.authenticationService.relationship().subscribe((res: any) => {
       this.data = res;
       this.authenticationService.relative(0).subscribe((res: any) => {
-        if(res){
+        if (res) {
           this.relative_data = res;
         }
       }, err => {
@@ -31,8 +34,9 @@ export class HomeComponent implements OnInit {
   onChange(event) {
     const newVal = event.target.value;
     this.authenticationService.relative(newVal).subscribe((res: any) => {
-      if(res){
+      if (res) {
         this.relative_data = res;
+        console.log(res)
       }
     }, err => {
       console.log(err);
@@ -40,12 +44,12 @@ export class HomeComponent implements OnInit {
   }
 
 
-  onDelete = (id:number, id2:number) => {
+  onDelete = (id: number, id2: number) => {
     this.authenticationService.deleterelative(id).subscribe(
       resdata => {
         console.log("delete successfully")
         this.authenticationService.relative(id2).subscribe((res: any) => {
-          if(res){
+          if (res) {
             this.relative_data = res;
           }
         }, err => {
@@ -53,8 +57,42 @@ export class HomeComponent implements OnInit {
         });
       },
       error => {
-        console.log("delete fail:::",error)
+        console.log("delete fail:::", error)
       }
     )
   };
+
+  confirmDelete() {
+    this.show = !this.show;
+    if (this.show) {
+      this.Edit = "Hide";
+    } else {
+      this.Edit = "Show";
+      this.authenticationService.relationship().subscribe((res: any) => {
+          this.data = res;
+        },
+        err => {
+          console.log(err);
+        });
+    }
+  }
+
+  onUpdate(event, relativeid: number, relationid: number) {
+    console.log(relativeid,relationid)
+    const newVal = event.target.value;
+    this.authenticationService.updaterelation(relativeid, newVal, ).subscribe(
+      responsedata => {
+        this.authenticationService.relative(relationid).subscribe((res: any) => {
+          if (res) {
+            this.relative_data = res;
+            this.show = !this.show;
+          }
+        }, err => {
+          console.log(err);
+        });
+      },
+      err => {
+        console.log(err);
+      });
+  }
 }
