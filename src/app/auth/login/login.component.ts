de0f54559c491ac197364466fec4e5f8a9989af4
siddharth-999 <siddharthrakholiya9@gmail.com>
2021-04-15 18:49:41 +0530

    added email validation

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -22,7 +22,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-      email: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]],
       password: ['', Validators.required]
   });
 }
